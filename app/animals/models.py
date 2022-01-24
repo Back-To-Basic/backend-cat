e0a9da2167c72cc209ba0b548b3ed2e89638816a
joohyun <elonmoon@gmail.com>
2022-01-24 17:58:29 +0900

    Add: [Animal] Animal, AnimalImage 모델 추가

--- app/animals/models.py
+++ app/animals/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
-# Create your models here.
+
+class Animal(models.Model):
+    name = models.CharField(verbose_name='이름', max_length=128)
+    created = models.DateTimeField(verbose_name='생성일시', auto_now_add=True)
+    updated = models.DateTimeField(verbose_name='수정일시', auto_now=True)
+
+
+class AnimalImage(models.Model):
+    seq = models.IntegerField(verbose_name='순서', default=0)
+    image = models.ImageField(verbose_name='사진', upload_to='%Y/animals/%m')
+    created = models.DateTimeField(verbose_name='생성일시', auto_now_add=True)
+    updated = models.DateTimeField(verbose_name='수정일시', auto_now=True)
+
+    animal = models.ForeignKey('animals.Animal', verbose_name='동물', on_delete=models.SET_NULL,
+                               related_name='image_set', related_query_name='images', null=True)
+
+
+
